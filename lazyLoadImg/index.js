97a964eb5601d6fa474d052277cd3d2204913050
XavierCode <haoyuace@163.com>
2017-04-27 22:42:51 +0800

    feat:图片懒加载

--- lazyLoadImg/index.js
+++ lazyLoadImg/index.js
@@ -1,27 +1,57 @@
 /*
     原生js实现版本
 */
-var lazyImgs = document.querySelectorAll('.lazyImg');
-var n = lazyImgs.length;
+!(function(){
 
-//首先执行一次
-loadLazyImgs();
+    LazyLoad.prototype._loadImgs = function(){
+        //如果图片都加载完毕 则不再执行
+        var _imgs = this.allImgs;
+        console.log(this.allImgs);
+        var seeHeight = document.documentElement.clientHeight;
+        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
 
-window.onscroll = loadLazyImgs;
-
-function loadLazyImgs() {
-    var seeHeight = document.documentElement.clientHeight;
-    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-
-    for (var i = 0; i < lazyImgs.length; i++ ) {
-        if (lazyImgs[i].offsetTop < seeHeight + scrollTop) {
-            //图片在视口中间
-            (function(i) {
-                setTimeout(function() {
-                    lazyImgs[i].src = lazyImgs[i].getAttribute('data-src');
-                    console.log('载入' + i);
-                }, 500)
-            })(i);
+        for (var i = _imgs.length - 1; i >= 0; i-- ) {
+            if (_imgs[i].offsetTop < seeHeight + scrollTop) {
+                //图片在视口中间
+                if (_imgs[i].getAttribute('src') === this.defaultImgName) {
+                    _imgs[i].src = _imgs[i].getAttribute('data-src');
+                    _imgs.splice(i, 1);
+                }
+            }
         }
     }
-}
+    function LazyLoad(imgName, className, delayTime) {
+        this.defaultImgName = imgName ? imgName : 'default.png';
+        this.defaultClassName = className ? className : '.lazyImg';
+        this.allImgs = [];
+        //imgs = [];
+
+        //初始化
+        var nodes = document.querySelectorAll(this.defaultClassName);
+        if (nodes.length == 0) return;
+
+        for (var i = 0;i < nodes.length;i++) {
+            this.allImgs.push(nodes[i]);
+        }
+
+        //首先执行一次
+        this._loadImgs.bind(this);
+
+        if (delayTime) {
+            var self = this;
+            var timer;
+            window.addEventListener('scroll', function() {
+                timer && clearTimeout(timer);
+                timer = setTimeout(function() {
+                    console.log(11);
+                    self._loadImgs();
+                }.bind(self), delayTime)
+            })
+        } else {
+            window.addEventListener('scroll', this._loadImgs.bind(this));
+        }
+
+    }
+    
+    window.LazyLoad = LazyLoad;
+})()
